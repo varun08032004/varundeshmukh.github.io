4c54aafa0d34073c50a7ede95045fbe2d25786e5
varundeshmukh <142887727+varun08032004@users.noreply.github.com>
2024-07-23 16:42:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleVisibility(id) {
+    var element = document.getElementById(id);
+    if (element.classList.contains('hidden')) {
+        element.classList.remove('hidden');
+    } else {
+        element.classList.add('hidden');
+    }
+}
